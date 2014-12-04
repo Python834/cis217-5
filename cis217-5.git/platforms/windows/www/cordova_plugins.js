118cb178817a8105a6336210461f1c32d08530f8
Python834 <pytho834@gmail.com>
2014-12-03 19:18:40 -0800

    Third Commit

--- cis217-5.git/platforms/windows/www/cordova_plugins.js
+++ cis217-5.git/platforms/windows/www/cordova_plugins.js
@@ -48,13 +48,22 @@ module.exports = [
         "merges": [
             ""
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.device": "0.2.12",
-    "org.apache.cordova.camera": "0.3.3"
+    "org.apache.cordova.camera": "0.3.3",
+    "org.apache.cordova.vibration": "0.3.11"
 }
 // BOTTOM OF METADATA
 });
